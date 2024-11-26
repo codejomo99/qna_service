85df28f5469de65ef6a4c65534bf0a3226958330
Jong-Min <codejomo99@gmail.com>
2024-11-26 15:36:54 +0900

    feat: 간단한 GET,POST 차이

--- src/main/java/com/exam/qna/HomeController.java
+++ src/main/java/com/exam/qna/HomeController.java
@@ -2,15 +2,62 @@ package com.exam.qna;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
 
-    @RequestMapping("/qna")
+    @GetMapping("/qna")
     @ResponseBody
-    public String showHome(){
+    public String showHome() {
         return "메인 시작";
     }
+
+    @RequestMapping("/test")
+    @ResponseBody
+    public String showTest() {
+        return """
+                <h1> 안녕하세요. </h1>
+                <input type="text" placeholder = "입력하세요"/>
+                """;
+    }
+
+
+    @GetMapping("/test/page1")
+    @ResponseBody
+    public String showGet() {
+        return """
+                <form method = "POST" action="/test/page2">
+                    <input type="number" name = "age" placeholder = "나이 입력" />
+                    <input type="submit" value = "page2로 Post 방식" />
+                </form>
+                """;
+    }
+
+    @GetMapping("/test/page2")
+    @ResponseBody
+    public String showPageGet(@RequestParam(defaultValue = "0") int age) {
+        return """
+                <h1>입력된 나이 : %d</h1>
+                <h1> GET 방식 </h1>
+                """.formatted(age);
+    }
+
+    // RequestParam defaultValue 를 통해 0 값의 오류를 막는다.
+    // http://localhost:8080/test/page2?age=10 -(GET)
+    // http://localhost:8080/test/page2 -(POST)
+    @PostMapping("/test/page2")
+    @ResponseBody
+    public String showPagePost(@RequestParam(defaultValue = "0") int age) {
+        return """
+                <h1>입력된 나이 : %d</h1>
+                <h1> POST 방식 </h1>
+                """.formatted(age);
+    }
+
+
 }
