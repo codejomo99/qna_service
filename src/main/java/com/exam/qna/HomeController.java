f21310d1c11541078c80dc91baf1983a8defa05e
Jong-Min <codejomo99@gmail.com>
2024-11-26 18:07:18 +0900

    feat: 간단한 게시글 추가 하고 확인하기

--- src/main/java/com/exam/qna/HomeController.java
+++ src/main/java/com/exam/qna/HomeController.java
@@ -11,6 +11,8 @@ import java.util.List;
 import java.util.Map;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
+import lombok.ToString;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,6 +23,14 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
+
+    private int increaseNum;
+    private List<Article> articles;
+
+    public HomeController() {
+        increaseNum = -1;
+        articles = new ArrayList<>();
+    }
 
     @GetMapping("/qna")
     @ResponseBody
@@ -74,7 +84,7 @@ public class HomeController {
     // GET 요청으로 14가 나온다.
     @GetMapping("/test/plus")
     @ResponseBody
-    public int plus(@RequestParam(defaultValue = "0") int a, @RequestParam(defaultValue = "0")  int b){
+    public int plus(@RequestParam(defaultValue = "0") int a, @RequestParam(defaultValue = "0") int b) {
         return a + b;
     }
 
@@ -84,37 +94,35 @@ public class HomeController {
     public void plusServlet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         int a = Integer.parseInt(req.getParameter("a"));
         int b = Integer.parseInt(req.getParameter("b"));
-        resp.getWriter().append(a+b+"");
+        resp.getWriter().append(a + b + "");
     }
-
-
-
 
     // 함수안에 지역변수로 두면 함수 생성주기로 값이 초기화 되기 때문에
     // 전역변수로 둬 호출될때마다 값이 증가하게 해준다.
-    private int increaseNum;
-    public HomeController(){
-        increaseNum = -1;
-    }
+
 
     @GetMapping("/test/increase")
     @ResponseBody
-    public int increase(){
+    public int increase() {
         increaseNum++;
         return increaseNum;
     }
 
     @GetMapping("/test/gugudan")
     @ResponseBody
-    public String gugudan(Integer dan, Integer limit){
+    public String gugudan(Integer dan, Integer limit) {
 
         String result = "";
 
-        if(dan == null) dan = 9;
-        if(limit == null) limit = 9;
+        if (dan == null) {
+            dan = 9;
+        }
+        if (limit == null) {
+            limit = 9;
+        }
 
-        for(int i = 1; i <= limit; i++){
-            result += "%d * %d = %d<br>".formatted(dan,i,dan*i);
+        for (int i = 1; i <= limit; i++) {
+            result += "%d * %d = %d<br>".formatted(dan, i, dan * i);
         }
         return result;
     }
@@ -125,8 +133,8 @@ public class HomeController {
     // switch 문을 변수에 담아서 사용 가능.
     @GetMapping("/test/mbti/{name}")
     @ResponseBody
-    public String showMbti(@PathVariable String name){
-        String rs = switch (name){
+    public String showMbti(@PathVariable String name) {
+        String rs = switch (name) {
             case "홍길동" -> "INFP";
             case "김하늘" -> "ENFP";
             case "박종민" -> "INTJ";
@@ -139,7 +147,6 @@ public class HomeController {
     }
 
 
-
     // 세션을 이용해서 값을 저장하기
     @GetMapping("/test/saveSession/{name}/{value}")
     @ResponseBody
@@ -148,46 +155,47 @@ public class HomeController {
 
         // req = > 쿠키 = > JESSIONID => 세션을 얻을 수 있다.
 
-        session.setAttribute(name,value);
+        session.setAttribute(name, value);
 
-        return "세션 변수 %s의 값이 %s(으)로 설정되었습니다.".formatted(name,value);
+        return "세션 변수 %s의 값이 %s(으)로 설정되었습니다.".formatted(name, value);
     }
 
     @GetMapping("/test/getSession/{name}")
     @ResponseBody
-    public String getSession(@PathVariable String name, HttpSession session){
+    public String getSession(@PathVariable String name, HttpSession session) {
         String value = (String) session.getAttribute(name);
 
-        return "세션 변수의 %s의 값이 %s입니다.".formatted(name,value);
+        return "세션 변수의 %s의 값이 %s입니다.".formatted(name, value);
     }
 
     /**
-     데이터 반환 타입과 메서드 공부
+     * 데이터 반환 타입과 메서드 공부
      **/
 
     @GetMapping("/test/returnBoolean")
     @ResponseBody
-    public boolean showBoolean(){
+    public boolean showBoolean() {
         return false;
     }
+
     @GetMapping("/test/returnDouble")
     @ResponseBody
-    public double showDouble(){
+    public double showDouble() {
         return Math.random();
     }
 
     @GetMapping("/test/returnIntArr")
     @ResponseBody
-    public int[] showIntArr(){
-        int[] arr = {1,3,5};
+    public int[] showIntArr() {
+        int[] arr = {1, 3, 5};
 
         return arr;
     }
 
     @GetMapping("/test/returnStringList")
     @ResponseBody
-    public List<String> showStringList(){
-        List<String> list = new ArrayList<>(){{
+    public List<String> showStringList() {
+        List<String> list = new ArrayList<>() {{
             add("안녕");
             add("테스트");
             add("입니다");
@@ -197,31 +205,28 @@ public class HomeController {
     }
 
 
-
     @GetMapping("/test/returnMap")
     @ResponseBody
-    public Map<String, Object> showMap(){
-       Map<String, Object> map = new LinkedHashMap<>(){{
-            put("id",1);
-            put("age",5);
-            put("name","푸바오");
-            put("list",new ArrayList<>(){{
+    public Map<String, Object> showMap() {
+        Map<String, Object> map = new LinkedHashMap<>() {{
+            put("id", 1);
+            put("age", 5);
+            put("name", "푸바오");
+            put("list", new ArrayList<>() {{
                 add(2);
                 add(3);
                 add(4);
             }});
-       }};
+        }};
 
-
-       return map;
+        return map;
     }
-
 
 
     @GetMapping("/test/returnAnimal")
     @ResponseBody
-    public Animal showAnimal(){
-        Animal animal = new Animal(1,3,"도니",new ArrayList<>(){{
+    public Animal showAnimal() {
+        Animal animal = new Animal(1, 3, "도니", new ArrayList<>() {{
             add(2);
             add(3);
             add(4);
@@ -232,23 +237,23 @@ public class HomeController {
 
     @GetMapping("/test/returnAnimalMapList")
     @ResponseBody
-    public List<Map<String, Object>> showAnimalMap(){
-        Map<String, Object> map1 = new LinkedHashMap<>(){{
-            put("id",1);
-            put("age",5);
-            put("name","푸바오");
-            put("list",new ArrayList<>(){{
+    public List<Map<String, Object>> showAnimalMap() {
+        Map<String, Object> map1 = new LinkedHashMap<>() {{
+            put("id", 1);
+            put("age", 5);
+            put("name", "푸바오");
+            put("list", new ArrayList<>() {{
                 add(2);
                 add(3);
                 add(4);
             }});
         }};
 
-        Map<String, Object> map2 = new LinkedHashMap<>(){{
-            put("id",2);
-            put("age",11);
-            put("name","도니");
-            put("list",new ArrayList<>(){{
+        Map<String, Object> map2 = new LinkedHashMap<>() {{
+            put("id", 2);
+            put("age", 11);
+            put("name", "도니");
+            put("list", new ArrayList<>() {{
                 add(5);
                 add(6);
                 add(7);
@@ -265,21 +270,21 @@ public class HomeController {
 
     @GetMapping("/test/returnAnimalList")
     @ResponseBody
-    public List<Animal> showAnimalList(){
+    public List<Animal> showAnimalList() {
 
-        Animal animal1 = new Animal(1,3,"도니",new ArrayList<>(){{
+        Animal animal1 = new Animal(1, 3, "도니", new ArrayList<>() {{
             add(2);
             add(3);
             add(4);
         }});
 
-        Animal animal2 = new Animal(2,14,"흰둥이",new ArrayList<>(){{
+        Animal animal2 = new Animal(2, 14, "흰둥이", new ArrayList<>() {{
             add(2);
             add(3);
             add(4);
         }});
 
-        List<Animal> list = new ArrayList<>(){{
+        List<Animal> list = new ArrayList<>() {{
             add(animal1);
             add(animal2);
         }};
@@ -287,17 +292,66 @@ public class HomeController {
         return list;
     }
 
+    // 간단한 게시판 구현
+    @GetMapping("/addArticle")
+    @ResponseBody
+    public String addArticle(String title, String body) {
+        Article article = new Article(title, body);
+
+        articles.add(article);
+
+        return "%d번 게시물이 추가되었습니다.".formatted(article.getId());
+    }
+
+    @GetMapping("/article/list")
+    @ResponseBody
+    public List<Article> getArticles(){
+        return articles;
+    }
+
+    @GetMapping("/article/detail/{id}")
+    @ResponseBody
+    public Article getArticle(@PathVariable int id){
+        Article article = articles.stream()
+                .filter(a -> a.getId() == id) // 게시물 ID와 내가 입력한 ID 일치하는지 확인
+                .findFirst() // 찾은것중 첫번째
+                .orElse(null); // 아무것도 없으면 null 반환
+
+
+        return article;
+    }
+
+
 
 }
 
 // 생성자만들어준다
 @AllArgsConstructor
 @Data
-class Animal{
+class Animal {
     private final int id;
     private final int age;
 
     private String name;
 
     private final List<Integer> list;
+}
+
+
+@AllArgsConstructor
+@Getter
+@ToString
+class Article {
+    private static int lastId;
+    private final int id;
+    private String title;
+    private String body;
+
+    static{
+        lastId = 0;
+    }
+
+    public Article(String title, String body){
+        this(++lastId, title, body); // 메서드 오버로딩
+    }
 }
