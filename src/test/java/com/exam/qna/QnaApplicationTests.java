927a193473d5d84cefd1ab2a8beb991886798507
Jong-Min <codejomo99@gmail.com>
2024-11-27 21:24:57 +0900

    chore: junit 의존성 추가

--- src/test/java/com/exam/qna/QnaApplicationTests.java
+++ src/test/java/com/exam/qna/QnaApplicationTests.java
@@ -1,13 +1,31 @@
 package com.exam.qna;
 
+import com.exam.qna.entity.Question;
+import com.exam.qna.repository.QuestionRepository;
+import java.time.LocalDateTime;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class QnaApplicationTests {
 
+	@Autowired
+	private QuestionRepository questionRepository;
+
 	@Test
-	void contextLoads() {
+	void testJpa() {
+		Question q1 = new Question();
+		q1.setSubject("이게 무엇인가요?");
+		q1.setContent("이것에 대해서 알고 싶습니다.");
+		q1.setCreateDate(LocalDateTime.now());
+		this.questionRepository.save(q1);  // 첫번째 질문 저장
+
+		Question q2 = new Question();
+		q2.setSubject("스프링부트 모델 질문입니다.");
+		q2.setContent("id는 자동으로 생성되나요?");
+		q2.setCreateDate(LocalDateTime.now());
+		this.questionRepository.save(q2);  // 두번째 질문 저장
 	}
 
 }
