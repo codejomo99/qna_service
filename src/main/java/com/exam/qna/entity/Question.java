0ce549744af3a5303c8f1947fe5869a996ba089f
Jong-Min <codejomo99@gmail.com>
2024-11-27 21:31:21 +0900

    test: 간단한 테스트케이스 추가

--- src/main/java/com/exam/qna/entity/Question.java
+++ src/main/java/com/exam/qna/entity/Question.java
@@ -9,8 +9,12 @@ import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import java.time.LocalDateTime;
 import java.util.List;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
 public class Question {
 
     @Id
