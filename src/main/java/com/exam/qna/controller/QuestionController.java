f1523d78bb735030669fea8a4314511fba01e615
Jong-Min <codejomo99@gmail.com>
2024-12-08 17:11:49 +0900

    style: 간단한 스타일 꾸미기

--- src/main/java/com/exam/qna/controller/QuestionController.java
+++ src/main/java/com/exam/qna/controller/QuestionController.java
@@ -24,8 +24,6 @@ public class QuestionController {
         // 미리 실행된 question_list.html 에서
         // questionList 라는 이름으로 questionList 변수를 사용 할 수 있다.
         model.addAttribute("questionList",questionsList);
-
-
         return "question_list";
     }
 
