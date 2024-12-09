fa0c50c83688c27ea05c61aaa75910b236307a6a
Jong-Min <codejomo99@gmail.com>
2024-12-09 16:51:53 +0900

    feat: 질문 생성

--- src/main/java/com/exam/qna/service/QuestionService.java
+++ src/main/java/com/exam/qna/service/QuestionService.java
@@ -3,6 +3,7 @@ package com.exam.qna.service;
 import com.exam.qna.entity.Question;
 import com.exam.qna.error.DataNotFoundException;
 import com.exam.qna.repository.QuestionRepository;
+import java.time.LocalDateTime;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -19,5 +20,13 @@ public class QuestionService {
     public Question getQuestion(int id) {
         return questionRepository.findById(id)
                 .orElseThrow(()-> new DataNotFoundException("no %d question not fund".formatted(id)));
+    }
+
+    public void create(String subject, String content) {
+        Question q = new Question();
+        q.setSubject(subject);
+        q.setContent(content);
+        q.setCreateDate(LocalDateTime.now());
+        questionRepository.save(q);
     }
 }
