521eca039a9c1fc054c4397aa491edc8590bb5f3
Jong-Min <codejomo99@gmail.com>
2024-12-09 17:41:19 +0900

    feat: 데이터 유효성 검사 -> validation으로 수정 예정

--- src/main/java/com/exam/qna/controller/QuestionController.java
+++ src/main/java/com/exam/qna/controller/QuestionController.java
@@ -43,8 +43,26 @@ public class QuestionController {
     }
 
     @PostMapping("/create")
-    public String questionCreate(String subject, String content){
-        questionService.create(subject,content);
+    public String questionCreate(Model model, QuestionForm questionForm){
+
+       boolean hasError = false;
+
+        if(questionForm.getSubject() == null || questionForm.getSubject().trim().isEmpty()){
+            model.addAttribute("subjectErrorMsg", "제목이 없어용");
+            hasError = true;
+        }
+
+        if(questionForm.getContent() == null || questionForm.getContent().trim().isEmpty()){
+            model.addAttribute("contentErrorMsg", "내용이 없어용");
+            hasError = true;
+        }
+
+        if(hasError){
+            model.addAttribute("questionForm",questionForm);
+            return "question_form";
+        }
+
+        questionService.create(questionForm.getSubject(), questionForm.getContent());
         return "redirect:/question/list";
     }
 }
