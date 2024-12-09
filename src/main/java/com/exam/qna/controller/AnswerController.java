0194bb4dd6e208c5fa6b6c02a76537a16f0c094c
Jong-Min <codejomo99@gmail.com>
2024-12-09 18:30:41 +0900

    feat: Answer에도 밸리데이션 적용

--- src/main/java/com/exam/qna/controller/AnswerController.java
+++ src/main/java/com/exam/qna/controller/AnswerController.java
@@ -3,9 +3,11 @@ package com.exam.qna.controller;
 import com.exam.qna.entity.Question;
 import com.exam.qna.service.AnswerService;
 import com.exam.qna.service.QuestionService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,10 +21,16 @@ public class AnswerController {
     private final AnswerService answerService;
 
     @PostMapping("/create/{id}")
-    public String createAnswer(Model model, @PathVariable int id, String content){
+    public String createAnswer(Model model, @PathVariable int id, @Valid AnswerForm answerForm, BindingResult bindingResult){
+
         Question question = questionService.getQuestion(id);
 
-        answerService.create(question,content);
+        if(bindingResult.hasErrors()){
+            model.addAttribute("question",question);
+            return "question_detail";
+        }
+
+        answerService.create(question,answerForm.getContent());
 
         return String.format("redirect:/question/detail/%s",id);
     }
