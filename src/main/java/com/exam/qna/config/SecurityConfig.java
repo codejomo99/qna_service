301993e7787521675ec0f8be3926a332d8de70c7
Jong-Min <codejomo99@gmail.com>
2024-12-11 17:16:25 +0900

    feat: 회원 Config 구현 #11

---
+++ src/main/java/com/exam/qna/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.exam.qna.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+
+// 시큐리티 설정
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((authorizeHttpRequests) -> authorizeHttpRequests
+                        .requestMatchers(new AntPathRequestMatcher("/**")) // 모든 경로를
+                        .permitAll()); // 허용한다
+
+
+        return http.build();
+    }
+}
