e7799bcdcfb66824c28e2912b5ce89a7a2fbc1c6
Jong-Min <codejomo99@gmail.com>
2024-12-13 15:21:47 +0900

    feat: 질문 수정폼 #16

--- src/main/java/com/exam/qna/controller/QuestionController.java
+++ src/main/java/com/exam/qna/controller/QuestionController.java
@@ -4,12 +4,14 @@ import com.exam.qna.dto.AnswerForm;
 import com.exam.qna.dto.QuestionForm;
 import com.exam.qna.entity.Question;
 import com.exam.qna.entity.SiteUser;
+import com.exam.qna.error.DataNotFoundException;
 import com.exam.qna.service.QuestionService;
 import com.exam.qna.service.SiteUserService;
 import jakarta.validation.Valid;
 import java.security.Principal;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -19,6 +21,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.server.ResponseStatusException;
 
 @Controller
 @RequestMapping("/question")
@@ -65,5 +68,22 @@ public class QuestionController {
 
         questionService.create(questionForm.getSubject(), questionForm.getContent(),siteUser);
         return "redirect:/question/list";
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("/modify/{id}")
+    public String questionModify(QuestionForm questionForm, @PathVariable("id") Long id, Principal principal) {
+        Question question = this.questionService.getQuestion(id);
+
+        if(question == null){
+            throw new DataNotFoundException("%d번 질문은 존재하지 않습니다.".formatted(id));
+        }
+
+        if(!question.getAuthor().getUsername().equals(principal.getName())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "수정권한이 없습니다.");
+        }
+        questionForm.setSubject(question.getSubject());
+        questionForm.setContent(question.getContent());
+        return "question_form";
     }
 }
