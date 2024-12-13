45a3e8ab4f5b16ff783e5d5a889912b0fcfda73d
Jong-Min <codejomo99@gmail.com>
2024-12-13 16:22:03 +0900

    feat: 답변 수정 #16

--- src/main/java/com/exam/qna/controller/AnswerController.java
+++ src/main/java/com/exam/qna/controller/AnswerController.java
@@ -1,6 +1,7 @@
 package com.exam.qna.controller;
 
 import com.exam.qna.dto.AnswerForm;
+import com.exam.qna.entity.Answer;
 import com.exam.qna.entity.Question;
 import com.exam.qna.entity.SiteUser;
 import com.exam.qna.service.AnswerService;
@@ -9,13 +10,16 @@ import com.exam.qna.service.SiteUserService;
 import jakarta.validation.Valid;
 import java.security.Principal;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.server.ResponseStatusException;
 
 @Controller
 @RequiredArgsConstructor
@@ -43,5 +47,36 @@ public class AnswerController {
 
         return String.format("redirect:/question/detail/%s",id);
     }
+
+
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("/modify/{id}")
+    public String answerModify(AnswerForm answerForm, @PathVariable("id") Long id, Principal principal) {
+        Answer answer = answerService.getAnswer(id);
+
+        if (!answer.getAuthor().getUsername().equals(principal.getName())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "수정권한이 없습니다.");
+        }
+
+        answerForm.setContent(answer.getContent());
+        return "answer_form";
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @PostMapping("/modify/{id}")
+    public String answerModify(Principal principal,@PathVariable Long id, @Valid AnswerForm answerForm, BindingResult bindingResult){
+
+        if(bindingResult.hasErrors()){
+            return "ansewr_form";
+        }
+        Answer answer = answerService.getAnswer(id);
+        if(!answer.getAuthor().getUsername().equals(principal.getName())){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST,"수정권한이 없습니다.");
+        }
+        answerService.modify(answer, answerForm.getContent());
+        return String.format("redirect:/question/detail/%s",answer.getQuestion().getId());
+
+    }
+
 
 }
